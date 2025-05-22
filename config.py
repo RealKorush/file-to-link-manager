fd1eef699ccad1f2789b0b2306982e794da7b2f1
RealKorush <68956993+RealKorush@users.noreply.github.com>
2025-05-22 03:45:43 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,41 @@
+BOT_TOKEN = "7676098241:AAGtvcM-G0aj77HnBB-tdSfpILLY4bb5v3I" # در عمل از متغیر محیطی استفاده کنید
+SUPER_ADMIN_USER_ID = 594976861
+SERVER_CAPACITY_GB = 2.5
+
+SERVERS = {
+    "server1": {
+        "access_key": "it9dlgvf7rriu0s1",
+        "secret_key": "04a97537-01a9-458e-a3f3-231fa7090372",
+        "bucket": "elated-faraday-t-tzyfahd",
+        "endpoint_url": "https://storage.c2.liara.space",
+        "region_name": "" # معمولا برای S3 compatible ها نیاز نیست یا مقدار خاصی دارد، داکیومنت لیارا را چک کنید
+    },
+    "server2": {
+        "access_key": "3l1f7cvpmhdmaom6",
+        "secret_key": "e52e16a8-9c4b-4ce1-9203-1608b231c537",
+        "bucket": "hardcore-volhard-i-safft-j",
+        "endpoint_url": "https://storage.iran.liara.space", # توجه کنید که این endpoint متفاوت است
+        "region_name": ""
+    },
+    "server3": {
+        "access_key": "95u2hsge5esiqpv6",
+        "secret_key": "c27f94b4-687b-4b62-962a-3bd824125b75",
+        "bucket": "xenodochial-villani-op-h8bewh",
+        "endpoint_url": "https://storage.c2.liara.space",
+        "region_name": ""
+    },
+    "server4": {
+        "access_key": "sq31595lcpk838kp",
+        "secret_key": "f2a508a2-7cb7-4dea-8ec9-9065aa2ab92a",
+        "bucket": "upbeat-leakey-lf4pap8mg",
+        "endpoint_url": "https://storage.c2.liara.space",
+        "region_name": ""
+    },
+    "server5": {
+        "access_key": "mufd6cbiboqc7drd",
+        "secret_key": "58d6c376-ad41-430b-9538-9484b62daf7b",
+        "bucket": "naughty-snyder-sg4jwocvw",
+        "endpoint_url": "https://storage.c2.liara.space",
+        "region_name": ""
+    }
+}
